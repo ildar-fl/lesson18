829d8ba57f3bb2da71556b3096d9ea9bb8c4526a
Ildar <ifaskhetdinov@goodwix.team>
2021-08-09 20:56:10 +0300

    feat: add donate-list

--- lessons19/src/modules/app.js
+++ lessons19/src/modules/app.js
@@ -1,14 +1,27 @@
 import { DonateForm } from './donate-form';
+import { DonaleList } from './donate-list';
+
+const mockDonates = [
+    { amount: 4, date: new Date() },
+    { amount: 20, date: new Date() },
+    { amount: 3, date: new Date() },
+    { amount: 1, date: new Date() },
+ ];
 
 class App {
     #donateForm
+    #donateList
 
     constructor() {
         this.#donateForm = new DonateForm();
+        this.#donateList = new DonaleList(mockDonates); 
     }
 
     run() {
-        document.body.append(this.#donateForm.render());
+        const donateFormHTML = this.#donateForm.render();
+        const donateListHTML = this.#donateList.render();
+
+        document.body.append(donateFormHTML, donateListHTML);
     }
 }
 
---
+++ lessons19/src/modules/donate-list.js
@@ -0,0 +1,45 @@
+
+class DonaleList {
+    #donatens
+    #container
+    #title
+    #donatesWrapper
+
+
+    constructor(donatens) {
+        this.#donatens = donatens;
+
+        this.#container = document.createElement('div');
+        this.#container.className = 'donates-container';
+
+        this.#title = document.createElement('h2');
+        this.#title.className = 'donates-container__title';
+        this.#title.textContent = 'Список донатов';
+
+        this.#donatesWrapper = document.createElement('div');
+        this.#donatesWrapper.className = 'donate-item';
+    }
+
+    #createDonate({ date, amount }) {
+        const donateContainer = document.createElement('div');
+        donateContainer.className = 'donate-item';
+        donateContainer.textContent = date;
+        
+        const amountContainer = document.createElement('b');
+        amountContainer.textContent = `${amount}$`;
+
+        donateContainer.append(amountContainer);
+
+        return donateContainer;
+    }
+
+    render() {
+      const donates = this.#donatens.map(this.#createDonate);
+      this.#donatesWrapper.append(...donates);
+      this.#container.append(this.#title, this.#donatesWrapper);  
+
+      return this.#container;
+    }
+}
+
+export { DonaleList }
