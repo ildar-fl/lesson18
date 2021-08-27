ab6ddbb1a59ad1f88aa8ca20c0b8fe28a8c4c716
Ildar <ifaskhetdinov@goodwix.team>
2021-08-27 20:48:41 +0300

    create table users

--- fast-company/src/components/users.js
+++ fast-company/src/components/users.js
@@ -1,8 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import api from "../api";
 
-const User = () => {
-  return <h1>Users</h1>;
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
+
+  const handleDelete = (userId) => {
+    setUsers((prev) => prev.filter(({ _id }) => userId !== _id));
+  };
+
+  const renderPhrase = (number) => {
+    if (number === 0) {
+      return <span class="badge bg-danger">Никто с тобой не тусанет</span>;
+    }
+
+    const countString =
+      number === 1 ? "1 человек тусанёт" : `${number} человек тусанут`;
+
+    return <span class="badge bg-info">{countString} с тобой чегодня</span>;
+  };
+
+  return (
+    <>
+      <h1>{renderPhrase(users.length)}</h1>
+      {users.length > 0 && (
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Профессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(
+              ({
+                _id,
+                name,
+                qualities,
+                profession,
+                completedMeetings,
+                rate,
+              }) => {
+                return (
+                  <tr key={_id}>
+                    <th scope="row">{name}</th>
+                    <td>
+                      {qualities.map(({ _id: badgeId, name, color }) => (
+                        <span key={badgeId} class={`badge bg-${color}`}>
+                          {name}
+                        </span>
+                      ))}
+                    </td>
+                    <td>{profession.name}</td>
+                    <td>{completedMeetings}</td>
+                    <td>{rate}</td>
+                    <td>
+                      <button
+                        className="btn btn-secondary bg-danger"
+                        onClick={() => handleDelete(_id)}
+                      >
+                        delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              }
+            )}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 };
 
-export default User;
+export default Users;
